9e391ec3044628d4d075fa29a42ecd7a202cba7e
zaydmk6 <zaydmk950@gmail.com>
2025-10-26 02:11:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,18 @@ URLS_LIKE = {"IND":"https://client.ind.freefiremobile.com/LikeProfile",
             "BR":"https://client.us.freefiremobile.com/LikeProfile",
             "US":"https://client.us.freefiremobile.com/LikeProfile",
             "SAC":"https://client.us.freefiremobile.com/LikeProfile",
-            "NA":"https://client.us.freefiremobile.com/LikeProfile"}
+            "NA":"https://client.us.freefiremobile.com/LikeProfile",
+            "ME":"https://clientbp.ggwhitehawk.com/LikeProfile"
+            }
 
 
 URLS_INFO = {"IND":"https://client.ind.freefiremobile.com/GetPlayerPersonalShow",
             "BR":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
             "US":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
             "SAC":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
-            "NA":"https://client.us.freefiremobile.com/GetPlayerPersonalShow"}
+            "NA":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
+            "ME":"https://clientbp.ggwhitehawk.com//GetPlayerPersonalShow"
+            }
 
 
 
@@ -19,4 +23,6 @@ FILES = {"IND":"token_ind.json",
              "BR":"token_br.json",
              "US":"token_br.json",
              "SAC":"token_br.json",
-             "NA":"token_br.json"}
+             "NA":"token_br.json",
+             "ME": "tokens.json"
+        }
